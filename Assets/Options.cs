665989419daa9d778664926670223a3a09d0b744
SENA SİMAY YASAR <101476251+simayasar@users.noreply.github.com>
2024-06-30 18:25:50 +0200

    correcting conflict added soil type

--- Assets/Options.cs
+++ Assets/Options.cs
@@ -22,6 +22,11 @@ public class OptionsMenu1 : MonoBehaviour
     public PipeGenerator pipeGenerator;
     public WaterGenerator waterGenerator;
     public Grid_Generator gridGenerator;
+    public Grid_GeneratorSp grid_GeneratorSp;
+    public Grid_GeneratorW grid_GeneratorW;
+    public Grid_GeneratorF grid_GeneratorF;
+    public Grid_GeneratorSum grid_GeneratorSum;
+    public TextureChanger textureChanger;
     private Light directionalLight;
     private Dropdown drowpdown;
     private TMP_Dropdown myDropdownWeather;
@@ -69,7 +74,15 @@ public class OptionsMenu1 : MonoBehaviour
         gridGenerator = GameObject.Find("XR Origin (XR Rig)").GetComponent<Grid_Generator>();
         Debug.Log("waterGenerator ready");
 
+        textureChanger = GameObject.Find("XR Origin (XR Rig)").GetComponent<TextureChanger>(); 
 
+        grid_GeneratorSp = GameObject.Find("XR Origin (XR Rig)").GetComponent<Grid_GeneratorSp>();
+
+        grid_GeneratorW = GameObject.Find("XR Origin (XR Rig)").GetComponent<Grid_GeneratorW>();
+
+        grid_GeneratorF = GameObject.Find("XR Origin (XR Rig)").GetComponent<Grid_GeneratorF>();
+
+        grid_GeneratorSum = GameObject.Find("XR Origin (XR Rig)").GetComponent<Grid_GeneratorSum>();
 
 
     }
@@ -90,9 +103,9 @@ public class OptionsMenu1 : MonoBehaviour
     }
     public void OnDropdownValueChanged(int index)
     {
+        DeactivateAllIrrigationSystems();
         //int selectedIndex = myDropdown.value;
         //TMP_Dropdown.OptionData selectedOption = myDropdownWeather.options[index];
-        
         //Debug.Log("Dropdown value changed! Index: " + index);
 
 
@@ -120,14 +133,14 @@ public class OptionsMenu1 : MonoBehaviour
         switch (myDropdownIrrigation.value)
         {
             case 0: // Drip
-                changeToDrip();
+                ActivateDrip();
                 Debug.Log("pipes activated");
                 break;
             case 1: // Sprinkler
                 Debug.Log("changed to sprinkler");
                 break;
             case 2: // Furrow
-                changeToFurrow();
+                ActivateFurrow();
                 Debug.Log("changed to furrow");
                 break;
             case 3: // Terraced
@@ -142,18 +155,26 @@ public class OptionsMenu1 : MonoBehaviour
             case 0: //Tomato
                 changeToTomato();
                 break;
-            case 1: // Potato
-                Debug.Log("no potato");
-                break;
-            case 2: // Aub
+      
+            case 1: // Aub
                 changeToAub();
                 break;
 
         }
 
-
-
-
+       
+        switch (myDropdownSoil.value)
+        {
+            case 0: // Sandy Soil
+                textureChanger.ChangeToLoamySoil();
+                break;
+            case 1: // Loamy Soil
+                textureChanger.ChangeToSandySoil();
+                break;
+            case 2: // Clay Soil
+               textureChanger.ChangeToClaySoil();
+                break;
+        }
 
     }
 
@@ -173,22 +194,69 @@ public class OptionsMenu1 : MonoBehaviour
     {
         SceneManager.LoadScene(6);
     }
-    private void changeToDrip()
+
+    private void DeactivateAllIrrigationSystems()
     {
-        //waterGenerator.DeactivateWater();
+        waterGenerator.DeactivateWater();
+        pipeGenerator.DeactivatePipes();
+        
+    }
+    private void ActivateDrip()
+    {
         pipeGenerator.GeneratePipes();
     }
-    private void changeToFurrow()
+
+    private void ActivateFurrow()
     {
         waterGenerator.GenerateWater();
     }
+
     private void changeToTomato() 
     {
-        gridGenerator.GenerateTomato();
-        Debug.Log("Changed to Tomato");
+        switch (SceneManager.GetActiveScene().buildIndex)
+        {
+            case 3: //Fall
+                grid_GeneratorF.GenerateTomato();
+                break;
+            case 4: // Summer
+                grid_GeneratorSum.GenerateTomato();
+                break;
+            case 5: // Winter
+                grid_GeneratorW.GenerateTomato();
+                break;
+            case 6: // Spring
+                grid_GeneratorSp.GenerateTomato();
+                break;
+            case 1:
+                gridGenerator.GenerateTomato();
+                break;
+
+
+
+        }
     }
     private void changeToAub()
     {
-        gridGenerator.GenerateAub();
+      
+        switch (SceneManager.GetActiveScene().buildIndex)
+        {
+            case 3: //Fall
+                grid_GeneratorF.GenerateAub();
+                break;
+            case 4: // Summer
+                grid_GeneratorSum.GenerateAub();
+                break;
+            case 5: // Winter
+                grid_GeneratorW.GenerateAub();
+                break;
+            case 6: // Spring
+                grid_GeneratorSp.GenerateAub();
+                break;
+            case 1:
+                gridGenerator.GenerateAub();
+                break;
+
+        }
     }
+    
 }
